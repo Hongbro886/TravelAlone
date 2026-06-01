43ceb2fa55ef742b30af492fff4378b508ebff66
Hongbro886 <hongbroyier@gmail.com>
2026-06-02 05:21:44 +0800

    添加核心原则与免责声明

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,7 +3,19 @@ import { defaultTheme } from '@vuepress/theme-default'
 import { defineUserConfig } from 'vuepress'
 
 export default defineUserConfig({
+  title: "一份独自出行的完全指南",
+  description: "当一个未成年人想要肚子在中国大陆出行时，相信这篇文档一定能帮到你",
   bundler: viteBundler(),
   theme: defaultTheme({
+    sidebar: [
+      {
+        text :"前语",
+        children:[
+          'PrePare/a.md',
+          'PrePare/b.md'
+        ]
+      }
+    ]
+
   }),
 })
--- docs/README.md
+++ docs/README.md
@@ -1,1 +1,5 @@
-# Hello VuePress
+---
+lang: zh-CN
+title: 这是什么
+description: 这篇文档的性质
+---
