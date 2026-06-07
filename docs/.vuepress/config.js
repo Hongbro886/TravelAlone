18468a3e592cfb684b561434b75006b735357d65
Hongbro886 <hongbroyier@gmail.com>
2026-06-08 05:41:59 +0800

    删除不合适的话，添加favicon

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -6,6 +6,11 @@ export default defineUserConfig({
   base: '/TravelAlone/',
   title: "一份独自出行的完全指南",
   description: "当一个未成年人想要肚子在中国大陆出行时，相信这篇文档一定能帮到你",
+  head: [
+    ['link', { rel: 'icon', href: '/TravelAlone/favicon.ico' }],
+    ['link', { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/TravelAlone/favicon-32x32.png' }],
+    ['link', { rel: 'apple-touch-icon', sizes: '180x180', href: '/TravelAlone/apple-touch-icon.png' }],
+  ],
   bundler: viteBundler(),
   theme: defaultTheme({
     sidebar: [
